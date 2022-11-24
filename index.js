2d08558e204e92a759c65f6d3cc666ee45f6b6dc
FELIX-MAIN <117909509+FELIX-MAIN@users.noreply.github.com>
2022-11-24 14:21:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+// code your solution here
+const record = [
+    {year: "2015", result: "w"},
+    {year: "2014", result: "N/A"},
+    {year: "2013", result: "l"},
+]
+
+const superbowlWin = record.find(record => record.year === '2015');
+
+console.log(superbowlWin);
+
